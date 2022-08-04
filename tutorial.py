2f9cd422ef9d397b9b231587130a21f00dfe4ec6
paulycarp <stev.paul@yahoo.com>
2022-08-04 09:32:15 -0700

    Changed 'home' to 'index':

--- tutorial.py
+++ tutorial.py
@@ -3,7 +3,7 @@ from flask import Flask
 app  = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
     return "Hello! World I am new here. Thank. <h1>Hello<h1>"
 
 if __name__ == "__main__":
