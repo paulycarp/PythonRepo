7e0cc6b049a75f26de74e499b7edade5b0a50e04
paulycarp <stev.paul@yahoo.com>
2022-08-05 04:56:21 -0700

    added another page in pythonclas

--- pythonclass.py
+++ pythonclass.py
@@ -6,5 +6,9 @@ app  = Flask(__name__)
 def index():
     return "Hello! World I am new here. Thank. <h1>Hello<h1>"
 
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
 if __name__ == "__main__":
     app.run()
