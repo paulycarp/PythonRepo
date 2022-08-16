e0a48678d3c99f5820395d4cc113d6b893da2556
paulycarp <stev.paul@yahoo.com>
2022-08-16 06:46:35 -0700

    python.py modigied

--- python.py
+++ python.py
@@ -4,12 +4,14 @@ from myfile import title
 import myfile
 import myfile
 import sys              #Load a library Module
+import random
 print(sys.platform)
 print(2**5)             #Raise 2 to power 5
 X = "Spam!"
 print(X*8)              #String repetition
 Days = "Today is Thursday"
 print(Days)
+power = len(str(2**1000000))       #To the lenght of returned value 
 Name = input("Enter your Name: ")
 Age = input("Enter your Age: ")
 newAge = int(Age) + float(25)
@@ -17,6 +19,9 @@ greating = input("Write a greating: ")
 print(greating)
 print(newAge)
 print(7)
+print(power)
+print(random.choice(['cassava', 'cocoyam', 'corn', 'potatoes', 'plantain', 'yam', 'igbagwu', 'okpa', 'beans', 'rice']))
+print(random.choice([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
 
 a = 3
 b = 5
@@ -27,17 +32,17 @@ print(a+b)
 #O = "Life"
 #print(M, N, O)
 
-print(20 - 10/5*3**2)
+#print(20 - 10/5*3**2)
 
-print((20-10)/5*3**2)
+#print((20-10)/5*3**2)
 
-print("spam!"*8)
+#print("spam!"*8)
 
-for x in 'spam!':
-    print(x)
+#for x in 'spam!':
+    #print(x)
 
-print(myfile.title)
-print(title)
-print(M, N, O)
-print(M, N)
+#print(myfile.title)
+#print(title)
+#print(M, N, O)
+#print(M, N)
 #print(myfile)
